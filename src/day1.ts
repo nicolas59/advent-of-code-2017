c0fbd0573f50043ec5747ab85e2e7233efb699b0
Rousseau Nicolas [Lille] <nicolas.rousseau1@gfi.fr>
2017-12-06 08:01:05 +0100

    Update tsconfig

--- src/day1.ts
+++ src/day1.ts
@@ -8,7 +8,7 @@ function sum(data: string) {
 }
 
 function sumHalfway(data: string){
-    var dataAr =  data.split("");
+    const dataAr =  data.split("");
     return dataAr
      .slice(0, dataAr.length/2)
      .map((val, index, array) => val === dataAr[index + dataAr.length/2]? 2 * parseInt(val):0)
